716ebd9c0bfb4bb7fb59f5d62a31697f65ec876d
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2024-02-05 00:28:15 +0100

    updates

--- src/App/Engine.js
+++ src/App/Engine.js
@@ -2,23 +2,9 @@ import {
     FEMOptionsForBothEndsFixed, 
     FEMOptionsForOneEndFixed, 
     obtainAntiClockWiseSlopeDeflectionEquation,
-    obtainClockWiseSlopeDeflectionEquation
+    obtainClockWiseSlopeDeflectionEquation,
+    loadingConditions
 } from './Utils'
-
-// SpanConditions
-const loadingConditions = [
-    'None.',
-    'Point load at center.',
-    'Point load at distance \'a\' from left end and \'b\' from the right end.',
-    'Two equal point loads, spaced at 1/3 of the total length from each other.',
-    'Three equal point loads, spaced at 1/4 of the total length from each other.',
-    'Uniformly distributed load over the whole length.',
-    'Uniformly distributed load over half of the span on the right side. ',
-    'Uniformly distributed load over half of the span on the left side.',
-    'Variably distributed load, with highest point on the right end.',
-    'Variably distributed load, with highest point on the left end.',
-    'Variably distributed load, with highest point at the centre.'
-]
 
 
 export const evaluate = (data) => {
@@ -108,7 +94,7 @@ export const evaluate = (data) => {
         }
 
         fixedEndMomentsAndSlopeDeflectionEquations.push(spanResult)
-        results.fixedEndMoments = [...fixedEndMomentsAndSlopeDeflectionEquations]
+        results.fixedEndMomentsAndSlopeDeflectionEquations = [...fixedEndMomentsAndSlopeDeflectionEquations]
         console.log(results)
     }
 }
--- src/App/Utils.js
+++ src/App/Utils.js
@@ -1,3 +1,18 @@
+// Loading conditions
+export const loadingConditions = [
+    'None.',
+    'Point load at center.',
+    'Point load at distance \'a\' from left end and \'b\' from the right end.',
+    'Two equal point loads, spaced at 1/3 of the total length from each other.',
+    'Three equal point loads, spaced at 1/4 of the total length from each other.',
+    'Uniformly distributed load over the whole length.',
+    'Uniformly distributed load over half of the span on the right side. ',
+    'Uniformly distributed load over half of the span on the left side.',
+    'Variably distributed load, with highest point on the right end.',
+    'Variably distributed load, with highest point on the left end.',
+    'Variably distributed load, with highest point at the centre.'
+]
+
 // FIXED END MOMENTS
 
 // - Both ends fixed
