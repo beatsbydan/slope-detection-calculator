3f123ad1c31c0df2851dce58ae7975aba7f67278
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2024-02-04 02:23:13 +0100

    updates

--- src/App/Utils.js
+++ src/App/Utils.js
@@ -1,20 +1,76 @@
-// Fixed End Moments Formulas
-
-export const FEMFormulas = {
-    distributedLoadOverSpan: +-((w * l ** 2) / 12),
-    pointLoadOverSpan: +-((-2 * p * l) / 9)
-}
-
-// Slope-detection-equation
-
-export const SlopeDetectionEquation = {
-    clockwise: clockwiseFEM + ((2 * E * I) / L) * ((2 * ThetaA) + (ThetaB) - ((3 * Triangle) / L)),
-    antiClockwise: antiClockwiseFEM + ((2 * E * I) / L) * ((2 * ThetaA) + (ThetaB) - ((3 * Triangle) / L)),
-}
-
 // Constant values
 
 export const Constants = {
     E: '',
     I: ''
 }
+
+// Fixed End Moments Formulas
+
+export const FEMForPointLoadAtCenter = (load, length) => {
+    return ((load * length) / 8)
+}
+
+export const FEMForPointLoadAtDistanceAFromLeftAndBFromRight = (load, length, a, b) => {
+    return (load * b ** 2 * a) / (length ** 2)
+}
+
+export const FEMForTwoEqualPointLoadsSpacedAtOneThirdOfTotalLengthFromEachOther = (load, length) => {
+    return (2 * load * length) / 9
+}
+
+export const FEMForThreeEqualPointLoadsSpacedAtOneFourthOfTotalLengthFromEachOther = (load, length) => {
+    return (15 * load * length) / 48
+}
+
+export const FEMForUniformlyDistributedLoadOverTheWholeLength = (load, length) => {
+    return (load * length ** 2) / 12
+}
+
+export const FEMForUniformlyDistributedLoadOveHalfOfTheSpanOnTheRightSide = (load, length) => {
+    return (load * length ** 2) / 12
+}
+
+export const LeftFEMForUniformlyDistributedLoadOveHalfOfTheSpanOnTheLeftSide = (load, length) => {
+    return (11 * load * length ** 2) / 192
+}
+
+export const RightFEMForUniformlyDistributedLoadOveHalfOfTheSpanOnTheLeftSide = (load, length) => {
+    return (5 * load * length ** 2) / 192
+}
+
+export const LeftFEMForUniformlyDistributedLoadOveHalfOfTheSpanOnTheRightSide = (load, length) => {
+    return (5 * load * length ** 2) / 192
+}
+
+export const RightFEMForUniformlyDistributedLoadOveHalfOfTheSpanOnTheRightSide = (load, length) => {
+    return (11 * load * length ** 2) / 192
+}
+
+export const LeftFEMForVariablyDistributedLoadWithHighestPointAtLeftSide = (load, length) => {
+    return (load * length ** 2) / 20
+}
+
+export const RightFEMForVariablyDistributedLoadWithHighestPointAtLeftSide = (load, length) => {
+    return (load * length ** 2) / 30
+}
+
+export const LeftFEMForVariablyDistributedLoadWithHighestPointRightSide = (load, length) => {
+    return (load * length ** 2) / 30
+}
+
+export const RightFEMForVariablyDistributedLoadWithHighestPointRightSide = () => {
+    return (load * length ** 2) / 20
+}
+
+export const FEMForVariablyDistributedLoadWithHighestPointCentre = (load, length) => {
+    return (5 * load * length ** 2) / 96
+}
+
+// Slope-detection-equation
+
+export const calculateSlopeDeflection = (fem, length, thetaA, thetaB, angularDisplacement) => {
+    const {E, I} = Constants
+    return (fem + (((2 * E * I) / length) * (thetaA + (2 * thetaB) +((3 * angularDisplacement) / length))))
+}
+
